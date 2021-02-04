8772912394d2ba23362f3b4ea7226a16bb5e8186
heathhartley <heathhartley55@gmail.com>
2021-02-03 18:03:22 -0700

    Add project files.

---
+++ Movies/Models/EnterMovie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Models
+{
+    public class EnterMovie
+    {
+        [StringLength(25, ErrorMessage ="You cannot enter more notes")]
+        public string note { get; set; }
+        public string rating { get; set; }
+        public string category { get; set; }
+        [Required]
+        public string title { get; set; }
+        [MinLength(2000)]
+        public int year { get; set; }
+        
+        [Required()]
+        public string director { get; set; }
+        public bool edited { get; set; }
+
+      
+    }
+}
---
+++ Movies/Models/tempstorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Models
+{
+    public static class tempstorage
+    {
+        private static List<EnterMovie> movies = new List<EnterMovie>();
+
+        public static IEnumerable<EnterMovie> EnterMovies => movies;
+
+        public static void AddMovie(EnterMovie movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
